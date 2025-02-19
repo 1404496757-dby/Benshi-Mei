81965c6c728731b18c29bcb5a39957023d9eca57
d1404496757 <d1404496757@outlook.com>
2025-02-19 09:53:32 +0800

    modified:   my_ctrller.egg-info/PKG-INFO 	modified:   my_ctrller/my_ctrller.py

--- my_ctrller/my_ctrller.py
+++ my_ctrller/my_ctrller.py
@@ -1,22 +1,40 @@
-from .base import Controller
-from .base import Action
-import numpy as np
-import pandas as pd
-import pkg_resources
-import logging
+from .user_interface import simulate
+from .base import Controller, Action
 
-logger = logging.getLogger(__name__)
-# CONTROL_QUEST = pkg_resources.resource_filename('simglucose',
-#                                                 'params/Quest.csv')
-# PATIENT_PARA_FILE = pkg_resources.resource_filename(
-#     'simglucose', 'params/vpatient_params.csv')
 
-class None_Controller(Controller):
-    def __int__(self):
-        pass
+class MyController(Controller):
+    def __init__(self, init_state):
+        self.init_state = init_state
+        self.state = init_state
 
     def policy(self, observation, reward, done, **info):
-        return Action(basal=0, bolus=0)
+        '''
+        Every controller must have this implementation!
+        ----
+        Inputs:
+        observation - a namedtuple defined in simglucose.simulation.env. For
+                      now, it only has one entry: blood glucose level measured
+                      by CGM sensor.
+        reward      - current reward returned by environment
+        done        - True, game over. False, game continues
+        info        - additional information as key word arguments,
+                      simglucose.simulation.env.T1DSimEnv returns patient_name
+                      and sample_time
+        ----
+        Output:
+        action - a namedtuple defined at the beginning of this file. The
+                 controller action contains two entries: basal, bolus
+        '''
+        self.state = observation
+        action = Action(basal=0, bolus=0)
+        return action
 
     def reset(self):
-        pass
+        '''
+        Reset the controller state to inital state, must be implemented
+        '''
+        self.state = self.init_state
+
+
+ctrller = MyController(0)
+simulate(controller=ctrller)
