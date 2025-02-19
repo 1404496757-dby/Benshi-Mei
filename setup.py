ae448be87977bcc796f3123131571e4af7f9bdc5
Benshi Mei <75458715+1404496757-dby@users.noreply.github.com>
2025-02-19 15:17:15 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,6 @@
 from distutils.core import setup
-setup(name="my_ctrller",version="0.1",author="Benshi_Mei")
+setup(name="my_ctrller",
+      version="0.1",
+      author="Benshi_Mei",
+      zip_safe=False,
+      )
